bc31b6bae19561c97780e297092d8999d596f912
Prajyot711 <cprajyot43@gmail.com>
2026-08-13 22:46:41 +0530

    connection Request API for send

--- src/models/user.js
+++ src/models/user.js
@@ -39,10 +39,9 @@ const userSchema = new mongoose.Schema({
     },
     gender: {
         type: String,
-        validate(value) {
-            if (!['male', 'female', 'others'].includes(value)) {
-                throw new Error("Gender is not valid!")
-            }
+        enum: {
+            values: ['male', 'female', 'others'],
+            message: `{VALUE} is not allowed gender!`
         }
     },
     about: {
--- src/routes/requests.js
+++ src/routes/requests.js
@@ -1,11 +1,50 @@
 const express = require("express");
-const {userAuth} = require("../middleware/userAuth.js")
-
+const {userAuth} = require("../middleware/userAuth.js");
+const ConnectionRequest = require("../models/connectionRequest.js");
+const user = require("../models/user.js");
 const requestRouter = express.Router();
 
-requestRouter.post("/sendConnectionRequest", userAuth, async (req,res) => {
+
+requestRouter.post("/request/send/:status/:userId", userAuth, async (req,res) => {
   try{
-    res.send("Connection Request send")
+    const toUserId = req.params.userId;
+    const fromUserId = req.user._id;
+    const status =  req.params.status;
+
+    const allowedStatus = ["interested", "ignored"];
+    if(!allowedStatus.includes(status)){
+      res.status(400).json({message: "Invalid status type " + status})
+    }
+
+    const toUser = await user.findById(toUserId)
+    if(!toUser){
+      return res.status(400).json({
+        message: "User Not Found!"
+      })
+    }
+
+    const existingConnectionRequest = await ConnectionRequest.findOne({
+      $or : [{toUserId,fromUserId},
+            {toUserId:fromUserId, fromUserId:toUserId}
+      ]
+    })
+    if(existingConnectionRequest){
+      res.status(400).send("message: Connection request already sexists!")
+    }
+    
+    const connectionRequest = new ConnectionRequest({
+      fromUserId,
+      toUserId,
+      status
+    })
+
+    const data = await connectionRequest.save();
+    res.json({
+      message:status === 'interested' ? `${req.user.firstName} is interested in ${toUser.firstName}` : `${req.user.firstName} is not interested in ${toUser.firstName}`,
+      data: data,
+    })
+
+
   }catch(err){
     res.status(400).send("ERROR: " + err.message)
   }
